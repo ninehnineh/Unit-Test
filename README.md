510d632add309103fd79c35391d7cda9b54dc938
ninehnineh <chinh.truongcong202@gmail.com>
2022-04-09 11:46:59 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # TestNG
 
+## We have two method to test, in both method we use TestNG DataProvider to pass the parameters in the test function, because we want to run the same test with multiple values (DDT)
+### Login
+Test case:
+* Test loginUser with valid value
+* Test loginUser with invalid value
+
+![login](https://github.com/ninehnineh/Unit-Test/blob/41949d8fe4f7192c0479d8ac41c2bd2df87e1632/screenshots/Login.png)
+### Insert
+Test case:
+* Test insertUser with valid value
+* Test insertUser with invalid value
+
+![Insert](https://github.com/ninehnineh/Unit-Test/blob/41949d8fe4f7192c0479d8ac41c2bd2df87e1632/screenshots/Insert.png)
 
--- test/user/DAONGTest.java
+++ test/user/DAONGTest.java
@@ -42,7 +42,7 @@ public class DAONGTest {
         return data;
     }
 
-    @Test(dataProvider = "data-provider")
+    @Test(dataProvider = "data-provider" , enabled = false)
     public void testInsertUserWithValidValue(String data, String data1, String data2, String data3, boolean boo) throws SQLException, Exception {
         DAO dao = new DAO();
         DTO user = new DTO(data, data1, data2, data3, boo);
@@ -51,7 +51,7 @@ public class DAONGTest {
         assertEquals(actual, expected);
     }
 
-    @Test(dataProvider = "data-provider")
+    @Test(dataProvider = "data-provider" , enabled = false)
     public void testInsertUserWithInvalidValue(String data, String data1, String data2, String data3, boolean boo) throws SQLException, Exception {
         DAO dao = new DAO();
         DTO user = new DTO(data, data1, data2, data3, boo);
@@ -68,7 +68,7 @@ public class DAONGTest {
         };
     }
 
-    @Test(dataProvider = "data-provider-valid", enabled = false)
+    @Test(dataProvider = "data-provider-valid")
     public void testLoginWithValidArgumentTest(String userid, String pass) throws SQLException, Exception {
         DAO dao = new DAO();
         DTO user = dao.checkLogin(userid, pass);
@@ -86,7 +86,7 @@ public class DAONGTest {
         };
     }
 
-    @Test(dataProvider = "data-provider-invalid", enabled = false)
+    @Test(dataProvider = "data-provider-invalid")
     public void testLoginWithInValidArgumentTest(String userid, String pass) throws SQLException, Exception {
         DAO dao = new DAO();
         DTO user = dao.checkLogin(userid, pass);
